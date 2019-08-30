7110adb23a8c3163337ba353c883c940f8e00880
Steven Paulino <stevenpaulino1@hotmail.com>
2019-08-30 13:55:03 -0400

    Two die rendering on click

--- src/Die.js
+++ src/Die.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Die.css";
 const die = props => {
-  console.log(props);
   return <i className={"Die fas fa-dice-" + props.num} />;
 };
 
--- src/RollDice.js
+++ src/RollDice.js
@@ -3,12 +3,43 @@ import React, { Component } from "react";
 import Die from "./Die";
 
 class RollDice extends Component {
+  state = {
+    die1: "one",
+    die2: "two",
+    sides: 6
+  };
+
+  rollDice = () => {
+    console.log("Rolling dice");
+
+    const die1 = this.props.dieSides[
+      this.getRandomInt(this.props.dieSides.length) - 1
+    ];
+    const die2 = this.props.dieSides[
+      this.getRandomInt(this.props.dieSides.length) - 1
+    ];
+
+    this.setState({ die1, die2 });
+  };
+
+  getRandomInt = max => {
+    return Math.ceil(Math.random() * Math.ceil(max));
+  };
+
   render() {
-    return <Die num={"five"} />;
+    return (
+      <React.Fragment>
+        <Die num={this.state.die1} />
+        <Die num={this.state.die2} />
+        <button onClick={this.rollDice}>Roll Dice</button>
+      </React.Fragment>
+    );
   }
 }
 
-export default RollDice;
+RollDice.defaultProps = {
+  dieSides: ["one", "two", "three", "four", "five", "six"]
+};
+//parseInt
 
-// state lives here
-// button click
+export default RollDice;
