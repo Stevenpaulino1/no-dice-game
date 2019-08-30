4376acdd292d0afa5a2075d51a8dbeedf4d918a8
Steven Paulino <stevenpaulino1@hotmail.com>
2019-08-30 17:27:31 -0400

    quic redirect guards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 import RollDice from "./RollDice";
 
 function App() {
   return (
     <div>
-      <RollDice />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={RollDice} />
+          <Redirect to="/" />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
