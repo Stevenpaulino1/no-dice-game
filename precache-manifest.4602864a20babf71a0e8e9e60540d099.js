cac896328a40dbf284d9bbba5acc975dbbb98711
Steven Paulino <stevenpaulino1@hotmail.com>
2019-08-30 18:30:10 -0400

    Updates

---
+++ precache-manifest.4602864a20babf71a0e8e9e60540d099.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a42185905b8cb4280f7688a37847df",
+    "url": "/nodice/index.html"
+  },
+  {
+    "revision": "d0d88b4e2f00a9ae8aad",
+    "url": "/nodice/static/css/main.73f1f0ab.chunk.css"
+  },
+  {
+    "revision": "77d23bd0dfee16b8a7cd",
+    "url": "/nodice/static/js/2.cd62414c.chunk.js"
+  },
+  {
+    "revision": "d0d88b4e2f00a9ae8aad",
+    "url": "/nodice/static/js/main.4c3864c4.chunk.js"
+  },
+  {
+    "revision": "0bb6a0ae14a98b108f27",
+    "url": "/nodice/static/js/runtime~main.ef0dae29.js"
+  }
+]);
